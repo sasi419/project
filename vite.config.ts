823d279f12fddd46218751c65625cb01b70c354e
sasi <sasi30379@gmail.com>
2025-06-05 18:42:01 +0530

    deploy the pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: '/S_star_fruitbite/', 
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
