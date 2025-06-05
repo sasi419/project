fb9f7c690625ab9c03e5c4373f3fe727c9334b63
sasi <sasi30379@gmail.com>
2025-06-05 18:29:53 +0530

    react

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 import AOS from 'aos';
 import Header from './components/Header';
 import HeroBanner from './components/HeroBanner';
